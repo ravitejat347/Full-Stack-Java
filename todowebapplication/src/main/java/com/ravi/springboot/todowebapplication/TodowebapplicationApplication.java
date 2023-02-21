8307fcc621b66cba67e2e42e9bdbc696782d23e2
raviteja085 <ravitejat347@gmail.com>
2023-02-21 00:50:32 -0500

    Spring MVC WebApp

---
+++ todowebapplication/src/main/java/com/ravi/springboot/todowebapplication/TodowebapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.springboot.todowebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodowebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodowebapplicationApplication.class, args);
+	}
+
+}
